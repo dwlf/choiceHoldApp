fcdde69d0685d7e6e0f184d0dce84934fbbc9900
Lloyd Dewolf <github@thedewofs.com>
2023-06-06 23:20:21 -0700

    Added X button to Add book view

--- ChoiceHold/AddBookView.swift
+++ ChoiceHold/AddBookView.swift
@@ -4,12 +4,12 @@ import CoreData
 struct AddBookView: View {
     @Environment(\.managedObjectContext) var moc
     @Environment(\.dismiss) var dismiss
-    
+
     @State private var title = ""
     @State private var author = ""
     @State private var isbn = ""
     @State private var publicationYear = 1999
-    
+
     var body: some View {
         NavigationView {
             Form {
@@ -23,13 +23,22 @@ struct AddBookView: View {
                         newBook.id = UUID()
                         newBook.title = title
                         newBook.author = author
-                        
+
                         try? moc.save()
                         dismiss()
                     }
                 }
             }
-        }.navigationTitle("Add Book")
-        
+            .navigationTitle("Add Book")
+            .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    Button(action: {
+                        dismiss()
+                    }) {
+                        Image(systemName: "xmark")
+                    }
+                }
+            }
+        }
     }
 }
